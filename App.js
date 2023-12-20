449a35a34fc2589d775516cc1d0a0ea656e9a616
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-12-21 01:03:35 +0630

    second

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Audio } from "expo-av";
+import { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component() {
+  async componentDidMount() {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      interruptionModeAndroid: Audio.INTERRRUPTION_MODE_ANDROID_DUCK_OTHERS,
+      shouldDuckAndroid: true,
+      staysActiveInBackground: true,
+      playThroughEarpieceAndroid: true,
+    });
+
+    this.sound = new Audio.Sound();
+
+    const status = {
+      shouldPlay: false,
+    };
+
+    this.sound.loadAsync(require("./assets/lofi_hiphop.mp3"), status, false);
+  }
+
+  playSound() {
+    this.sound.loadAsync();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Play sound"
+          color="#3cbbb1"
+          onPress={this.playsound.bind(this)}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
